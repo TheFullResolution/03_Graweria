62bb2de472274c89969e14f4d8677fb0e42ad359
TheFullResolution <progreacja@gmail.com>
2016-03-07 22:55:49 +0100

    feat: finished info, added map, added youtube

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,4 +1,4 @@
-/* global Backbone, _ */
+/* global Backbone, _, google */
 
 var app = {
     Video: function () {
@@ -49,14 +49,28 @@ var app = {
                 $('#video_div').addClass('video_div--hidden');
             });
 
+        } else {
+            app.router.navigate('info', {trigger: true});
         }
     }
+};
+
+app.mapLoader = function () {
+
+    var url = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDGUROHjSwHDzFbfvD07N-47_bq3bdgaOs&libraries=places';
+    $.ajax({
+        url: url,
+        dataType: 'script',
+        success: function () {
+            app.map.mapSuccess(true);
+        }});
 };
 
 app.Router = Backbone.Router.extend({
     'routes': {
         '': 'intro',
         'info': 'info',
+        'info/video': 'popup',
         'offer': 'offer',
         'map': 'map'
     },
@@ -65,12 +79,33 @@ app.Router = Backbone.Router.extend({
     },
     info: function () {
         app.info.render();
+        app.popup.clean();
+        $('.links').each(function () {
+            $(this).removeClass('link-selected');
+        });
+        $('.infolink').addClass('link-selected');
+    },
+    popup: function () {
+
+        app.popup.render();
+        $('.links').each(function () {
+            $(this).removeClass('link-selected');
+        });
+        $('.infolink').addClass('link-selected');
     },
     offer: function () {
-        console.log('ME TOO!');
+        $('.links').each(function () {
+            $(this).removeClass('link-selected');
+        });
+        $('.offerlink').addClass('link-selected');
     },
     map: function () {
         app.map.render();
+        $('.links').each(function () {
+            $(this).removeClass('link-selected');
+        });
+        $('.maplink').addClass('link-selected');
+
     }
 });
 
@@ -80,7 +115,20 @@ app.InfoView = Backbone.View.extend({
     el: $('#app'),
     template: _.template($('#info').html()),
     render: function () {
+        this.$el.html(this.template()).hide().fadeIn("slow");
+    }
+
+});
+
+app.PopUp = Backbone.View.extend({
+    el: $('#popup'),
+    template: _.template($('#popup_template').html()),
+    render: function () {
         this.$el.html(this.template());
+        $('.popup_mask, .popup_container').addClass('popup-enter').fadeTo("slow", 1);
+    },
+    clean: function () {
+        this.$el.html('');
     }
 
 });
@@ -88,11 +136,71 @@ app.InfoView = Backbone.View.extend({
 app.MapView = Backbone.View.extend({
     el: $('#app'),
     template: _.template($('#map').html()),
+    initialize: function () {
+        this.status = false;
+        this.on("changed:status", this.initMap, this);
+    },
     render: function () {
-        this.$el.html(this.template());
-    }
-});
+        this.$el.html('');
+        this.$el.html(this.template()).fadeIn("slow");
+        this.initMap();
+    },
+    initMap: function () {
+        if (this.status && document.getElementById('map_div')) {
+            map = new google.maps.Map(document.getElementById('map_div'), {
+                center: {lat: 54.51250049999999, lng: 18.539694699999927},
+                zoom: 15
+            });
+            service = new google.maps.places.PlacesService(map);
+            var marker = new google.maps.Marker({
+                map: map
+            });
+            var infowindow = new google.maps.InfoWindow();
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+            marker.setPlace({
+                placeId: 'ChIJZQ3glTGn_UYRn6wPMserIKY',
+                location: {lat: 54.51250049999999, lng: 18.539694699999927}
+            });
 
+            marker.setVisible(true);
+
+            var infoWindowRun = function () {
+                if (infowindow) {
+                    infowindow.close();
+                }
+                service.getDetails({
+                    placeId: 'ChIJZQ3glTGn_UYRn6wPMserIKY'
+                }, function (place, status) {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        var text = '<h3>' +
+                                place.name + '</h3>' +
+                                '<p>' + place.formatted_address + '</p>';
+                        infowindow = new google.maps.InfoWindow({
+                            content: text,
+                            maxWidth: 220
+                        });
+                        infowindow.open(map, marker);
+                    }
+                });
+
+            };
+
+            marker.addListener('click', function () {
+                infoWindowRun();
+            });
+
+        }
+    },
+    mapSuccess: function (value) {
+        this.status = value;
+        this.trigger("changed:status");
+    }
+
+
+
+});
 app.OfferViewItem = Backbone.Model.extend({});
 
 app.OfferViewCollection = Backbone.Collection.extend({});
@@ -103,8 +211,10 @@ app.OfferViewList = Backbone.View.extend({});
 
 
 $(function () {
+    app.mapLoader();
     app.router = new app.Router();
     app.info = new app.InfoView();
     app.map = new app.MapView();
+    app.popup = new app.PopUp();
     Backbone.history.start();
 });
